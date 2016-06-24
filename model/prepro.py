5ab1996855e5124cf317c6f026ae2eb160c71af6
Minjoon Seo <seominjoon@gmail.com>
2016-06-23 17:23:44 -0700

    put more info in prepro.py

--- model/prepro.py
+++ model/prepro.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 import os
+from collections import OrderedDict
 
 
 def get_args():
@@ -9,12 +11,35 @@ def get_args():
     target_dir = "data/model"
     parser.add_argument("--source_dir", default=source_dir)
     parser.add_argument("--target_dir", default=target_dir)
+    # TODO : put more args here
     return parser.parse_args()
 
 
 def prepro(args):
-    # TODO : do something
-    pass
+    source_dir = args.source_dir
+    target_dir = args.target_dir
+    # TODO : put something here
+    mode2idxs_dict = {}
+    data = {}
+    _save(mode2idxs_dict, data, target_dir)
+
+
+def _save(mode2idxs_dict, data, target_dir):
+    mode2idxs_path = os.path.join(target_dir, "mode2idxs.json")
+    metadata_path = os.path.join(target_dir, "metadata.json")
+    data_path = os.path.join(target_dir, "data.json")
+    if os.path.exists(metadata_path):
+        with open(metadata_path, 'r') as fh:
+            metadata = json.load(fh)
+    else:
+        metadata = OrderedDict()
+
+    with open(mode2idxs_path, 'w') as fh:
+        json.dump(fh, mode2idxs_dict)
+    with open(metadata_path, 'w') as fh:
+        json.dump(fh, metadata)
+    with open(data_path, 'w') as fh:
+        json.dump(fh, data)
 
 
 def main():
